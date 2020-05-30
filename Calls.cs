995594703f490fab5edbf6effb3e19ca4097c8cc
saschahi <39934087+saschahi@users.noreply.github.com>
2020-05-30 11:59:54 +0200

    V1.0.1

--- Calls.cs
+++ Calls.cs
@@ -1,9 +1,5 @@
 ﻿using ProjectT;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.ModLoader;
 
 namespace twitchtestmod
